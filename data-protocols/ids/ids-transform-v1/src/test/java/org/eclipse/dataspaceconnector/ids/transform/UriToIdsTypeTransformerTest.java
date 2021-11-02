2a5f1a50972556236f61e38dfe0352d48db5a975
Dominik Pinsel <dominik.pinsel@daimler.com>
2021-11-02 14:12:45 +0100

    ids: add UriToIdsTypeTransformerTest

--- data-protocols/ids/ids-transform-v1/src/test/java/org/eclipse/dataspaceconnector/ids/transform/UriToIdsTypeTransformerTest.java
+++ data-protocols/ids/ids-transform-v1/src/test/java/org/eclipse/dataspaceconnector/ids/transform/UriToIdsTypeTransformerTest.java
@@ -1,30 +1,27 @@
 package org.eclipse.dataspaceconnector.ids.transform;
 
 import org.easymock.EasyMock;
-import org.eclipse.dataspaceconnector.ids.spi.IdsId;
 import org.eclipse.dataspaceconnector.ids.spi.IdsType;
 import org.eclipse.dataspaceconnector.ids.spi.transform.TransformerContext;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.net.URI;
-
-public class UriToIdsIdTransformerTest {
+public class UriToIdsTypeTransformerTest {
 
     private static final IdsType IDS_ID_TYPE = IdsType.ARTIFACT;
     private static final String IDS_ID_VALUE = "32d39d70-68f7-44f3-b8b2-27550f2081f4";
-    private static final URI URI = java.net.URI.create("urn:artifact:32d39d70-68f7-44f3-b8b2-27550f2081f4");
+    private static final java.net.URI URI = java.net.URI.create("urn:artifact:32d39d70-68f7-44f3-b8b2-27550f2081f4");
 
     // subject
-    UriToIdsIdTransformer uriToIdsIdTransformer;
+    UriToIdsTypeTransformer uriToIdsTypeTransformer;
 
     // mocks
     private TransformerContext context;
 
     @BeforeEach
     public void setup() {
-        uriToIdsIdTransformer = new UriToIdsIdTransformer();
+        uriToIdsTypeTransformer = new UriToIdsTypeTransformer();
         context = EasyMock.createMock(TransformerContext.class);
     }
 
@@ -33,7 +30,7 @@ public class UriToIdsIdTransformerTest {
         EasyMock.replay(context);
 
         Assertions.assertThrows(NullPointerException.class, () -> {
-            uriToIdsIdTransformer.transform(null, null);
+            uriToIdsTypeTransformer.transform(null, null);
         });
     }
 
@@ -42,7 +39,7 @@ public class UriToIdsIdTransformerTest {
         EasyMock.replay(context);
 
         Assertions.assertThrows(NullPointerException.class, () -> {
-            uriToIdsIdTransformer.transform(URI, null);
+            uriToIdsTypeTransformer.transform(URI, null);
         });
     }
 
@@ -50,26 +47,21 @@ public class UriToIdsIdTransformerTest {
     void testReturnsNull() {
         EasyMock.replay(context);
 
-        var result = uriToIdsIdTransformer.transform(null, context);
+        var result = uriToIdsTypeTransformer.transform(null, context);
 
         Assertions.assertNull(result);
     }
 
     @Test
     void testSuccessfulSimple() {
-        //prepare
-        var idsId = IdsId.Builder.newInstance().type(IDS_ID_TYPE).value(IDS_ID_VALUE).build();
-        EasyMock.expect(context.transform(EasyMock.eq(URI), EasyMock.eq(IdsId.class))).andReturn(idsId);
-
         // record
         EasyMock.replay(context);
-        
+
         // invoke
-        var result = uriToIdsIdTransformer.transform(URI, context);
+        var result = uriToIdsTypeTransformer.transform(URI, context);
 
         // verify
         Assertions.assertNotNull(result);
-        Assertions.assertEquals(IDS_ID_TYPE, result.getType());
-        Assertions.assertEquals(IDS_ID_VALUE, result.getValue());
+        Assertions.assertEquals(IDS_ID_TYPE, result);
     }
 }
