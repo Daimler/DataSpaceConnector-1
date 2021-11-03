d5b671c228e9de07848f8e9db710b3a314817678
Denis Neuling <denis.neuling@daimler.com>
2021-11-03 14:53:07 +0100

    ids: Add license headers
    Signed-off-by: Denis Neuling <denis.neuling@daimler.com>

--- data-protocols/ids/ids-core/src/main/java/org/eclipse/dataspaceconnector/ids/core/configuration/SettingDefaults.java
+++ data-protocols/ids/ids-core/src/main/java/org/eclipse/dataspaceconnector/ids/core/configuration/SettingDefaults.java
@@ -1,9 +1,23 @@
+/*
+ *  Copyright (c) 2021 Daimler TSS GmbH
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Daimler TSS GmbH - Initial API and Implementation
+ *
+ */
+
 package org.eclipse.dataspaceconnector.ids.core.configuration;
 
 import org.eclipse.dataspaceconnector.ids.spi.IdsId;
 import org.eclipse.dataspaceconnector.ids.spi.types.SecurityProfile;
 
-public final class SettingDefaults {
+final class SettingDefaults {
     public static final String EDC_IDS_ID = "urn:connector:edc";
     public static final String EDC_IDS_TITLE = "Eclipse Dataspace Connector";
     public static final String EDC_IDS_DESCRIPTION = "Eclipse Dataspace Connector";
